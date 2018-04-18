55be8be397641de5a33684448a99204ba7b5aef6
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-04-18 08:41:35 +0200

    First commit
    First commit section 7
    
    [FA-19]

---
+++ README.md
@@ -0,0 +1,11 @@
+# Section 6
+
+Introduction to Flask-Restful library with an item API.
+
+## Run the code
+
+```
+$ virtualenv venv python=python3
+$ source venv/bin/activate
+$ npm start
+```
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_jwt import JWT
+from flask_restful import Api
+
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from security import authenticate, identity
+from resources.user import UserRegister
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.secret_key = 'jose'
+api = Api(app)
+
+
+@app.before_first_request
+def init_bbdd():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    from db import db
+
+    db.init_app(app)
+    app.run(port=5002, debug=True)
